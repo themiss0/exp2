1465cd685aabc9a9ccfe1ca0adeae1e2714e47b5
wool <sunset_shadows@qq.com>
2024-10-29 16:48:16 +0800

    添加了动作

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,280 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.7.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionNew;
+    QAction *actionOpen;
+    QAction *actionSave;
+    QAction *actionSaveAs;
+    QAction *actionExit;
+    QAction *actionUndo;
+    QAction *actionRedo;
+    QAction *actionCut;
+    QAction *actionCopy;
+    QAction *actionPaste;
+    QAction *actionFind;
+    QAction *actionReplace;
+    QAction *actionSelectAll;
+    QAction *actionAutoChangeRow;
+    QAction *actionZoomIn;
+    QAction *actionZoomOut;
+    QAction *actionReturnDefaultZoom;
+    QAction *actionShowStatusBar;
+    QAction *actionAbout;
+    QWidget *centralwidget;
+    QMenuBar *menubar;
+    QMenu *file;
+    QMenu *edit;
+    QMenu *type;
+    QMenu *view;
+    QMenu *menu_Z;
+    QMenu *help;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(800, 600);
+        actionNew = new QAction(MainWindow);
+        actionNew->setObjectName("actionNew");
+        QIcon icon(QIcon::fromTheme(QIcon::ThemeIcon::DocumentNew));
+        actionNew->setIcon(icon);
+        actionNew->setMenuRole(QAction::MenuRole::NoRole);
+        actionOpen = new QAction(MainWindow);
+        actionOpen->setObjectName("actionOpen");
+        QIcon icon1(QIcon::fromTheme(QIcon::ThemeIcon::DocumentOpen));
+        actionOpen->setIcon(icon1);
+        actionOpen->setMenuRole(QAction::MenuRole::NoRole);
+        actionSave = new QAction(MainWindow);
+        actionSave->setObjectName("actionSave");
+        QIcon icon2(QIcon::fromTheme(QIcon::ThemeIcon::DocumentSave));
+        actionSave->setIcon(icon2);
+        actionSave->setMenuRole(QAction::MenuRole::NoRole);
+        actionSaveAs = new QAction(MainWindow);
+        actionSaveAs->setObjectName("actionSaveAs");
+        QIcon icon3(QIcon::fromTheme(QIcon::ThemeIcon::DocumentSaveAs));
+        actionSaveAs->setIcon(icon3);
+        actionSaveAs->setMenuRole(QAction::MenuRole::NoRole);
+        actionExit = new QAction(MainWindow);
+        actionExit->setObjectName("actionExit");
+        QIcon icon4(QIcon::fromTheme(QIcon::ThemeIcon::WindowClose));
+        actionExit->setIcon(icon4);
+        actionExit->setMenuRole(QAction::MenuRole::NoRole);
+        actionUndo = new QAction(MainWindow);
+        actionUndo->setObjectName("actionUndo");
+        QIcon icon5(QIcon::fromTheme(QIcon::ThemeIcon::EditUndo));
+        actionUndo->setIcon(icon5);
+        actionUndo->setMenuRole(QAction::MenuRole::NoRole);
+        actionRedo = new QAction(MainWindow);
+        actionRedo->setObjectName("actionRedo");
+        QIcon icon6(QIcon::fromTheme(QIcon::ThemeIcon::EditRedo));
+        actionRedo->setIcon(icon6);
+        actionRedo->setMenuRole(QAction::MenuRole::NoRole);
+        actionCut = new QAction(MainWindow);
+        actionCut->setObjectName("actionCut");
+        QIcon icon7(QIcon::fromTheme(QIcon::ThemeIcon::EditCut));
+        actionCut->setIcon(icon7);
+        actionCut->setMenuRole(QAction::MenuRole::NoRole);
+        actionCopy = new QAction(MainWindow);
+        actionCopy->setObjectName("actionCopy");
+        QIcon icon8(QIcon::fromTheme(QIcon::ThemeIcon::EditCopy));
+        actionCopy->setIcon(icon8);
+        actionCopy->setMenuRole(QAction::MenuRole::NoRole);
+        actionPaste = new QAction(MainWindow);
+        actionPaste->setObjectName("actionPaste");
+        QIcon icon9(QIcon::fromTheme(QIcon::ThemeIcon::EditPaste));
+        actionPaste->setIcon(icon9);
+        actionPaste->setMenuRole(QAction::MenuRole::NoRole);
+        actionFind = new QAction(MainWindow);
+        actionFind->setObjectName("actionFind");
+        QIcon icon10(QIcon::fromTheme(QIcon::ThemeIcon::EditFind));
+        actionFind->setIcon(icon10);
+        actionFind->setMenuRole(QAction::MenuRole::NoRole);
+        actionReplace = new QAction(MainWindow);
+        actionReplace->setObjectName("actionReplace");
+        QIcon icon11(QIcon::fromTheme(QString::fromUtf8("sync-synchronizing")));
+        actionReplace->setIcon(icon11);
+        actionReplace->setMenuRole(QAction::MenuRole::NoRole);
+        actionSelectAll = new QAction(MainWindow);
+        actionSelectAll->setObjectName("actionSelectAll");
+        actionSelectAll->setMenuRole(QAction::MenuRole::NoRole);
+        actionAutoChangeRow = new QAction(MainWindow);
+        actionAutoChangeRow->setObjectName("actionAutoChangeRow");
+        actionAutoChangeRow->setMenuRole(QAction::MenuRole::NoRole);
+        actionZoomIn = new QAction(MainWindow);
+        actionZoomIn->setObjectName("actionZoomIn");
+        QIcon icon12(QIcon::fromTheme(QIcon::ThemeIcon::ZoomIn));
+        actionZoomIn->setIcon(icon12);
+        actionZoomIn->setMenuRole(QAction::MenuRole::NoRole);
+        actionZoomOut = new QAction(MainWindow);
+        actionZoomOut->setObjectName("actionZoomOut");
+        QIcon icon13(QIcon::fromTheme(QIcon::ThemeIcon::ZoomOut));
+        actionZoomOut->setIcon(icon13);
+        actionZoomOut->setMenuRole(QAction::MenuRole::NoRole);
+        actionReturnDefaultZoom = new QAction(MainWindow);
+        actionReturnDefaultZoom->setObjectName("actionReturnDefaultZoom");
+        actionReturnDefaultZoom->setMenuRole(QAction::MenuRole::NoRole);
+        actionShowStatusBar = new QAction(MainWindow);
+        actionShowStatusBar->setObjectName("actionShowStatusBar");
+        actionShowStatusBar->setMenuRole(QAction::MenuRole::NoRole);
+        actionAbout = new QAction(MainWindow);
+        actionAbout->setObjectName("actionAbout");
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 800, 20));
+        file = new QMenu(menubar);
+        file->setObjectName("file");
+        edit = new QMenu(menubar);
+        edit->setObjectName("edit");
+        type = new QMenu(menubar);
+        type->setObjectName("type");
+        view = new QMenu(menubar);
+        view->setObjectName("view");
+        menu_Z = new QMenu(view);
+        menu_Z->setObjectName("menu_Z");
+        help = new QMenu(menubar);
+        help->setObjectName("help");
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(file->menuAction());
+        menubar->addAction(edit->menuAction());
+        menubar->addAction(type->menuAction());
+        menubar->addAction(view->menuAction());
+        menubar->addAction(help->menuAction());
+        file->addAction(actionOpen);
+        file->addAction(actionNew);
+        file->addAction(actionSave);
+        file->addAction(actionSaveAs);
+        file->addAction(actionExit);
+        edit->addAction(actionUndo);
+        edit->addSeparator();
+        edit->addAction(actionCut);
+        edit->addAction(actionRedo);
+        edit->addAction(actionCopy);
+        edit->addAction(actionPaste);
+        edit->addAction(actionFind);
+        edit->addAction(actionReplace);
+        edit->addSeparator();
+        edit->addAction(actionSelectAll);
+        type->addAction(actionAutoChangeRow);
+        view->addAction(menu_Z->menuAction());
+        view->addAction(actionShowStatusBar);
+        menu_Z->addAction(actionZoomIn);
+        menu_Z->addAction(actionZoomOut);
+        menu_Z->addAction(actionReturnDefaultZoom);
+        help->addAction(actionAbout);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        actionNew->setText(QCoreApplication::translate("MainWindow", "\346\226\260\345\273\272(&N)", nullptr));
+#if QT_CONFIG(shortcut)
+        actionNew->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+N", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionOpen->setText(QCoreApplication::translate("MainWindow", "\346\211\223\345\274\200(&O)", nullptr));
+#if QT_CONFIG(shortcut)
+        actionOpen->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+O", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionSave->setText(QCoreApplication::translate("MainWindow", "\344\277\235\345\255\230(&S)", nullptr));
+#if QT_CONFIG(shortcut)
+        actionSave->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+S", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionSaveAs->setText(QCoreApplication::translate("MainWindow", "\345\217\246\345\255\230\344\270\272", nullptr));
+        actionExit->setText(QCoreApplication::translate("MainWindow", "\351\200\200\345\207\272(&Q)", nullptr));
+        actionUndo->setText(QCoreApplication::translate("MainWindow", "\346\222\244\351\224\200(&U)", nullptr));
+#if QT_CONFIG(shortcut)
+        actionUndo->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+Z", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionRedo->setText(QCoreApplication::translate("MainWindow", "\346\201\242\345\244\215", nullptr));
+#if QT_CONFIG(shortcut)
+        actionRedo->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+Y", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionCut->setText(QCoreApplication::translate("MainWindow", "\345\211\252\345\210\207(&T)", nullptr));
+#if QT_CONFIG(shortcut)
+        actionCut->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+X", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionCopy->setText(QCoreApplication::translate("MainWindow", "\345\244\215\345\210\266(&C)", nullptr));
+#if QT_CONFIG(shortcut)
+        actionCopy->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+C", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionPaste->setText(QCoreApplication::translate("MainWindow", "\347\262\230\350\264\264(&P)", nullptr));
+#if QT_CONFIG(shortcut)
+        actionPaste->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+V", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionFind->setText(QCoreApplication::translate("MainWindow", "\346\237\245\346\211\276(&F)", nullptr));
+#if QT_CONFIG(shortcut)
+        actionFind->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+F", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionReplace->setText(QCoreApplication::translate("MainWindow", "\346\233\277\346\215\242(&R)", nullptr));
+#if QT_CONFIG(shortcut)
+        actionReplace->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+H", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionSelectAll->setText(QCoreApplication::translate("MainWindow", "\345\205\250\351\200\211(&A)", nullptr));
+#if QT_CONFIG(shortcut)
+        actionSelectAll->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+A", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionAutoChangeRow->setText(QCoreApplication::translate("MainWindow", "\350\207\252\345\212\250\346\215\242\350\241\214", nullptr));
+        actionZoomIn->setText(QCoreApplication::translate("MainWindow", "\346\224\276\345\244\247(&I)", nullptr));
+#if QT_CONFIG(shortcut)
+        actionZoomIn->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+=", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionZoomOut->setText(QCoreApplication::translate("MainWindow", "\347\274\251\345\260\217(&O)", nullptr));
+#if QT_CONFIG(shortcut)
+        actionZoomOut->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+-", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionReturnDefaultZoom->setText(QCoreApplication::translate("MainWindow", "\346\201\242\345\244\215\351\273\230\350\256\244\347\274\251\346\224\276", nullptr));
+#if QT_CONFIG(shortcut)
+        actionReturnDefaultZoom->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+0", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionShowStatusBar->setText(QCoreApplication::translate("MainWindow", "\347\212\266\346\200\201\346\240\217(&S)", nullptr));
+        actionAbout->setText(QCoreApplication::translate("MainWindow", "\345\205\263\344\272\216", nullptr));
+        file->setTitle(QCoreApplication::translate("MainWindow", "\346\226\207\344\273\266(&F)", nullptr));
+        edit->setTitle(QCoreApplication::translate("MainWindow", "\347\274\226\350\276\221(&E)", nullptr));
+        type->setTitle(QCoreApplication::translate("MainWindow", "\346\240\274\345\274\217(&O)", nullptr));
+        view->setTitle(QCoreApplication::translate("MainWindow", "\346\237\245\347\234\213(&V)", nullptr));
+        menu_Z->setTitle(QCoreApplication::translate("MainWindow", "\347\274\251\346\224\276(&Z)", nullptr));
+        help->setTitle(QCoreApplication::translate("MainWindow", "\345\270\256\345\212\251(&H)", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
