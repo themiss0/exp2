c03322b2223bfcf7a2c808524cd7d3e509debd23
wool <sunset_shadows@qq.com>
2024-10-29 15:20:43 +0800

    添加了ignore

--- build/ui_mainwindow.h
+++ build/ui_mainwindow.h
@@ -25,7 +25,11 @@ class Ui_MainWindow
 public:
     QWidget *centralwidget;
     QMenuBar *menubar;
-    QMenu *menuhello_world;
+    QMenu *file;
+    QMenu *edit;
+    QMenu *type;
+    QMenu *view;
+    QMenu *help;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *MainWindow)
@@ -39,14 +43,26 @@ public:
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
         menubar->setGeometry(QRect(0, 0, 800, 20));
-        menuhello_world = new QMenu(menubar);
-        menuhello_world->setObjectName("menuhello_world");
+        file = new QMenu(menubar);
+        file->setObjectName("file");
+        edit = new QMenu(menubar);
+        edit->setObjectName("edit");
+        type = new QMenu(menubar);
+        type->setObjectName("type");
+        view = new QMenu(menubar);
+        view->setObjectName("view");
+        help = new QMenu(menubar);
+        help->setObjectName("help");
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName("statusbar");
         MainWindow->setStatusBar(statusbar);
 
-        menubar->addAction(menuhello_world->menuAction());
+        menubar->addAction(file->menuAction());
+        menubar->addAction(edit->menuAction());
+        menubar->addAction(type->menuAction());
+        menubar->addAction(view->menuAction());
+        menubar->addAction(help->menuAction());
 
         retranslateUi(MainWindow);
 
@@ -56,7 +72,11 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
-        menuhello_world->setTitle(QCoreApplication::translate("MainWindow", "hello world", nullptr));
+        file->setTitle(QCoreApplication::translate("MainWindow", "\346\226\207\344\273\266(&F)", nullptr));
+        edit->setTitle(QCoreApplication::translate("MainWindow", "\347\274\226\350\276\221(&E)", nullptr));
+        type->setTitle(QCoreApplication::translate("MainWindow", "\346\240\274\345\274\217(&O)", nullptr));
+        view->setTitle(QCoreApplication::translate("MainWindow", "\346\237\245\347\234\213(&V)", nullptr));
+        help->setTitle(QCoreApplication::translate("MainWindow", "\345\270\256\345\212\251(&H)", nullptr));
     } // retranslateUi
 
 };
